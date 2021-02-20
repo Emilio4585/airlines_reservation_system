4a045c5d44e62cf03127ec43bfaeebcaae3f5ab4
Emily <emily.moraima.cruz@hotmail.com>
2021-02-20 18:14:34 -0500

    asdf

--- models/Role/models.py
+++ models/Role/models.py
@@ -2,12 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from modulo import db
 
-class User(db.Model):
-    _id = db.Column("id", db.Integer, primary_key = True, )
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    address = db.Column(db.String(200))
-    phone = db.Column(db.Integer)
+
+class Role_model(db.Model): 
+    _id = db.Column("id", db.Integer, primary_key = True )
+    title = db.Column(db.String(100))
+    descriptcion = db.Column(db.String(250))
 
     def __repr__(self):
         return "<Name: {}>".format(self.name)
+
+
+
+class Role():
+    pass
