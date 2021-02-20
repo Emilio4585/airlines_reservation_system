66c59f0019134375fca842c295ad7501c0cf25dd
emilio4585 <emiliosalas54@gmail.com>
2021-02-20 16:33:38 -0600

    added model passenger

--- models/Passenger/models.py
+++ models/Passenger/models.py
@@ -1,13 +1,13 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from modulo import db
 
-class User(db.Model):
-    _id = db.Column("id", db.Integer, primary_key = True, )
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    address = db.Column(db.String(200))
-    phone = db.Column(db.Integer)
+class Passenger(db.Model):
+    passenger_id = db.Column("id", db.Integer, primary_key = True, )
+    passenger_name = db.Column(db.String(100))
+    passenger_mobile = db.Column(db.String(100))
+    passenger_email = db.Column(db.String(100))
+    passenger_username = db.Column(db.String(200))
+    passenger_password = db.Column(db.String(100))
+    passenger_adress = db.Column(db.String(100))
 
     def __repr__(self):
-        return "<Name: {}>".format(self.name)
+        return "<Name: {}>".format(self.passenger_name)
