b6b9cc83f5cf6e25a303ba8a9e9c4e352989ba23
Emily <emily.moraima.cruz@hotmail.com>
2021-02-20 20:13:26 -0500

    Aun falta actualizar el models de permission

--- models/Permission/models.py
+++ models/Permission/models.py
@@ -1,13 +1,43 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from modulo import db
 
-class User(db.Model):
-    _id = db.Column("id", db.Integer, primary_key = True, )
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    address = db.Column(db.String(200))
-    phone = db.Column(db.Integer)
+class Permission(db.Model):
+    permission_id = db.Column("id", db.Integer, primary_key = True, )
+    permission_role_id = db.Column(db.Integer)
+    permission_title = db.Column(db.String(100))
+    permission_module = db.Column(db.String(500))
+    permission_description = db.Column(db.String(500))
+
+
+    def addPermission(self, aid): # Add a Permission
+        self.permission_id = aid.get("permission_id", "None")
+        self.permission_role_id = aid.get("permission_role_id", "None")
+        self.permission_title = aid.get("permission_title", "None")
+        self.permission_module = aid.get("permission_module", "None")
+        self.permission_description = aid.get("permission_description", "None")
+        db.session.add(self)
+        db.session.commit()
+
+    def editRole(self, ad): # Edit Role
+        self.query.filter_by(role_id=ad).first()
+        self.role_id = ad.get("role_id", "None")
+        self.role_title = ad.get("role_name", "None")
+        self.role_description = ad.get("role_description", "None")
+        db.session.add(self)
+        db.session.commit()
+
+    def deleteRole(self, ai): # Delete a role
+        self.query.filter_by(role_id=ai).first()
+        self.session.delete()
+        self.session.commit()
+
+    def searchRole(self, eid): # Search for a role
+        return self.query.filter_by(role_id=eid).first()
+        
+    def assignRole(self, ed):
+        self.role_id = ed.get("role_id", "None")
+        self.role_title = ed.get("role_name", "None")
+        self.role_description = ed.get("role_description", "None")
+        db.session.commit()
 
     def __repr__(self):
-        return "<Name: {}>".format(self.name)
+        return "<Name: {}>".format(self.passenger_name)
